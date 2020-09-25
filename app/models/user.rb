89ab07f33d7c92a1fdd53daabd12facd166405b1
take321 <70375053+take321@users.noreply.github.com>
2020-09-25 18:23:57 +0900

    エラーメッセージの出力

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,6 @@ class User < ApplicationRecord
   validates :nickname, presence: true
   validates :email, presence: true, uniqueness: true, format: { with: /@.+/}
   validates :password, presence: true, length: { minimum: 6 }
-  validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください'
-  validates :password_confimation, presence: true
+  validates_format_of :password, {with: PASSWORD_REGEX}
+  validates :password_confirmation, presence: true
 end
