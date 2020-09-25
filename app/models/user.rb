d75f1d9eb3db6fc75899fcf17d59000849a44198
take321 <70375053+take321@users.noreply.github.com>
2020-09-25 16:07:14 +0900

    ユーザー情報のバリデーション

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates :nickname, presence: true
+  validates :email, presence: true, uniqueness: true, format: { with: /@.+/}
+  validates :password, presence: true, length: { minimum: 6 }
+  validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください'
+  validates :password_confimation, presence: true
 end
--- db/migrate/20200924034450_devise_create_users.rb
+++ db/migrate/20200924034450_devise_create_users.rb
@@ -5,7 +5,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
     create_table :users do |t|
       ## Database authenticatable
       t.string :nickname,           null: false, default: ""
-      t.string :email,              null: false, default: ""
+      t.string :email,              null: false, default: "" , uniqueness: true
       t.string :encrypted_password, null: false, default: ""
       t.string :family_name,        null: false
       t.string :family_name_kana,   null: false
