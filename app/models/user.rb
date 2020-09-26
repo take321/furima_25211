e3fc069ab7f18545271a5d0cd1f72ce0fd8b26c0
take321 <70375053+take321@users.noreply.github.com>
2020-09-26 11:13:00 +0900

    バリデーション修正と追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,10 +4,13 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
   validates :nickname, presence: true
   validates :email, presence: true, uniqueness: true, format: { with: /@.+/}
-  validates :password, presence: true, length: { minimum: 6 }
-  validates_format_of :password, {with: PASSWORD_REGEX}
+  validates :password, presence: true, format: {with: /\A[a-z\d]{6,100}+\z/i}
   validates :password_confirmation, presence: true
+  validates :family_name, presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]+\z/}
+  validates :first_name, presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]+\z/}
+  validates :family_name_kana, presence: true, format: {with: /\A[ァ-ン]+\z/}
+  validates :first_name_kana, presence: true, format: {with: /\A[ァ-ン]+\z/}
+  validates :birthday, presence: true
 end
