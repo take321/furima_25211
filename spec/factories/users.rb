b693921a6e5c69142e6565f1f07e1b7dbf03deba
take321 <70375053+take321@users.noreply.github.com>
2020-09-28 14:40:32 +0900

    factorybot記入

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,14 @@
 FactoryBot.define do
   factory :user do
-    
+    gimei = Gimei.name
+    nickname             {Faker::Name.initials}
+    email                {Faker::Internet.free_email}
+    password             {Faker::Internet.password(min_length: 6)}
+    password_confimation {password}
+    family_name          {gimei.last.kanji}
+    first_name           {gimei.first.kanji}
+    family_name_kana     {gimei.last.katakana}
+    first_name_kana      {gimei.first.katakana}
+    birthday             {Faker::Date.between}
   end
 end
