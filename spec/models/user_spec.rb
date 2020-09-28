83bcb4e48b0f7253ab077f85d81d61888a079d80
take321 <70375053+take321@users.noreply.github.com>
2020-09-28 18:41:27 +0900

    テストコード正常系

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,29 +6,48 @@ RSpec.describe User, type: :model do
   describe 'ユーザー新規登録' do
     context '新規登録がうまくいくとき' do
       it "全ての欄が埋まっていれば登録できる" do
+        expect(@user).to be_valid
       end
       it "emailに@を含んでいれば登録できる" do
+        @user.email = "abc@abc"
+        expect(@user).to be_valid
       end
-      it "passwordを半角英数字で書いていれば登録できる" do
-      end
-      it "passwordを6文字以上で書いていれば登録できる" do
+      it "passwordを半角英数字混合で6文字以上で書いていれば登録できる" do
+        @user.password = "abcde1"
+        @user.password_confirmation = @user.password
+        expect(@user).to be_valid
       end
       it "family_nameをひらがな、カタカナ、漢字で書けば登録できる" do
+        @user.family_name = "あア阿"
+        expect(@user).to be_valid
       end
       it "first_nameをひらがな、カタカナ、漢字で書けば登録できる" do
+        @user.first_name = "あア阿"
+        expect(@user).to be_valid
       end
       it "family_name_kanaをカタカナで書けば登録できる" do
+        @user.family_name_kana = "タロウ"
+        expect(@user).to be_valid
       end
       it "first_name_kanaをカタカナで書けば登録できる" do
+        @user.first_name_kana = "タナカ"
+        expect(@user).to be_valid
       end
     end
 
     context '新規登録がうまくいかないとき' do
       it "nicknameが空だと登録できない" do
+        @user.nickname = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Nickname can't be blank")
       end
       it "emailが空だと登録できない" do
+        @user.email = nil
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Email can't be blank")
       end
       it "emailが重複していた場合登録できない" do
+        
       end
       it "emailに@を含まなかった場合登録できない" do
       end
