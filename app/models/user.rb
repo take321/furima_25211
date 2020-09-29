486c749bb626d8166fa86e003877ca7422d59ecb
take321 <70375053+take321@users.noreply.github.com>
2020-09-29 11:10:02 +0900

    テストコード異常系、バリデーションの修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,8 +5,8 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname, presence: true
-  validates :email, presence: true, uniqueness: true, format: { with: /@.+/}
-  validates :password, presence: true, format: {with: /\A[a-z\d]{6,100}+\z/i}
+  validates :email, presence: true, uniqueness: {case_sensitive: true}, format: { with: /@.+/}
+  validates :password, presence: true, format: {with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i}
   validates :password_confirmation, presence: true
   validates :family_name, presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]+\z/}
   validates :first_name, presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]+\z/}
