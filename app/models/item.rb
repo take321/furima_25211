a652af14ffc99be4cf1f148b98e6fd051398d02a
take321 <70375053+take321@users.noreply.github.com>
2020-09-29 18:22:02 +0900

    shipping_feeモデルの作成

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,4 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to_active_hash :category
+  belongs_to_active_hash [:category,:condition,:shipping_fee,]
 end
