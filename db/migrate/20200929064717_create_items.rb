40613d03bb671210644f2b1dfc9cc7f43818e32b
take321 <70375053+take321@users.noreply.github.com>
2020-09-29 16:26:24 +0900

    itemsテーブルの作成

---
+++ db/migrate/20200929064717_create_items.rb
@@ -0,0 +1,15 @@
+class CreateItems < ActiveRecord::Migration[6.0]
+  def change
+    create_table :items do |t|
+      t.string  :name,              null: false
+      t.text    :text,              null: false
+      t.integer :category_id,       null: false
+      t.integer :condition_id,      null: false
+      t.integer :shipping_fee_id,   null: false
+      t.integer :delivery_days_id,  null: false
+      t.integer :price,             null: false
+      t.integer :user_id,           null: false, foreign_key: true
+      t.timestamps
+    end
+  end
+end
