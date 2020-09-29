f74ec20af5d19b0e62b52f123468c4783bb2a8d5
take321 <70375053+take321@users.noreply.github.com>
2020-09-29 19:34:40 +0900

    バリデーションの設定

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,13 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
+with_options presence: true do
+  validates :name
+  validates :text
+  validates :category_id
+  validates :condition_id
+  validates :shipping_fee_id
+  validates :shipping_area_id
+  validates :delivery_day_id
+  validates :price, numericality: { greater_than:299,less_than:10000000 }
+end
 end
