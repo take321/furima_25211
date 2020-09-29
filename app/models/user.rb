a8a7b40eb39c36f6157b08f726107fe4c9bda313
take321 <70375053+take321@users.noreply.github.com>
2020-09-29 11:42:19 +0900

    バリデーション簡略化

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,13 +4,15 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :nickname, presence: true
-  validates :email, presence: true, uniqueness: { case_sensitive: true }, format: { with: /@.+/ }
-  validates :password, presence: true, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i }
-  validates :password_confirmation, presence: true
-  validates :family_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/ }
-  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/ }
-  validates :family_name_kana, presence: true, format: { with: /\A[ァ-ン]+\z/ }
-  validates :first_name_kana, presence: true, format: { with: /\A[ァ-ン]+\z/ }
-  validates :birthday, presence: true
+  with_options presence: true do
+    validates :nickname
+    validates :email, uniqueness: { case_sensitive: true }, format: { with: /@.+/ }
+    validates :password,format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i }
+    validates :password_confirmation
+    validates :family_name, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/ }
+    validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/ }
+    validates :family_name_kana,format: { with: /\A[ァ-ン]+\z/ }
+    validates :first_name_kana, format: { with: /\A[ァ-ン]+\z/ }
+    validates :birthday
+  end
 end
