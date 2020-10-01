ac5af3962a736ca5c75fd1da93be3f64f4173046
take321 <70375053+take321@users.noreply.github.com>
2020-10-01 14:44:31 +0900

    factorybot fakerの設定

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,16 @@
+require 'faker'
+Faker::Config.locale = :ja
+
 FactoryBot.define do
   factory :item do
-    
+    name             {Faker::Restaurant.name}
+    text             {Faker::Restaurant.description}
+    category_id      {Faker::Number.within(range: 2..10)}
+    condition_id     {Faker::Number.within(range: 2..10)}
+    shipping_fee_id  {Faker::Number.within(range: 2..10)}
+    shipping_area_id {Faker::Number.within(range: 2..10)}
+    delivery_day_id  {Faker::Number.within(range: 2..10)}
+    price            {Faker::Number.within(range: 300..9999999)}
+    image            {Faker::Internet.url}
   end
 end
