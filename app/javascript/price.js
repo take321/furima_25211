c9986aced02534b8854121862af825da7c93f4d1
take321 <70375053+take321@users.noreply.github.com>
2020-10-01 12:01:39 +0900

    javascript微修正

--- app/javascript/price.js
+++ app/javascript/price.js
@@ -1,12 +1,12 @@
 function display(){
   const price = document.getElementById("item-price")
   price.addEventListener("keyup", () => {
-    let itemPrice = price.value;
+    const itemPrice = price.value;
     const addTaxPriceForm = document.getElementById("add-tax-price");
     const profitForm = document.getElementById("profit");
-    let addTaxPrice = itemPrice*0.1;
-    let profit = itemPrice*0.9;
-    addTaxPriceForm.innerHTML = `${parseInt(addTaxPrice, 10)}`;
+    const addTaxPrice = itemPrice*0.1;
+    const profit = itemPrice*0.9;
+    addTaxPriceForm.innerHTML = parseInt(addTaxPrice, 10);
     profitForm.innerHTML = parseInt(profit, 10);
   });
 }
