f7a3a7f55b0022f2ca443d18b9d3c75a2de41e25
take321 <70375053+take321@users.noreply.github.com>
2020-10-01 19:08:29 +0900

    テストコードの記述

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,47 +1,75 @@
 require 'rails_helper'
 RSpec.describe Item, type: :model do
   before do
-    @items = FactoryBot.build(:item)
+    @item = FactoryBot.build(:item)
   end
   describe '商品出品機能' do
     context '商品出品機能がうまくいくとき' do
       it '全ての欄が埋まっていれば登録できる' do
-        expect(@items).to be_valid
+        expect(@item).to be_valid
       end
     end
 
     context '商品出品機能が登録できないとき' do
       it '商品名がないと登録できない' do
+        @item.name = ""
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Name can't be blank")
       end
       it '商品の説明がないと登録できない' do
+        @item.text = ""
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Text can't be blank")
       end
       it 'カテゴリーが選択されていないと登録できない' do
-      end
-      it 'カテゴリーは選択するプルダウンが初期状態だと登録できない' do
+        @item.category_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Category must be other than 1")
       end
       it '商品の状態について選択されていないと登録できない' do
-      end
-      it '商品の状態はプルダウンが初期状態だと登録できない' do
+        @item.condition_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Condition must be other than 1")
       end
       it '配送料の負担について選択されていないと登録できない' do
-      end
-      it '配送料の負担はプルダウンが初期状態だと登録できない' do
+        @item.shipping_fee_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Shipping fee must be other than 1")
       end
       it '発送元の地域について選択されていないと登録できない' do
-      end
-      it '発送元の地域はプルダウンが初期状態だと登録できない' do
+        @item.shipping_area_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Shipping area must be other than 1")
       end
       it '発送までの日数について選択されていないと登録できない' do
-      end
-      it '発送までの日数はプルダウンが初期状態だと登録できない' do
+        @item.delivery_day_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Delivery day must be other than 1")
       end
       it '価格がないと登録できない' do
+        @item.price = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price can't be blank")
       end
-      it '画像がないと登録できない' do
+      it '価格が300円以下だと登録できない' do
+        @item.price = 299
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price must be greater than 299")
       end
-      it '価格が300円から1000万の間でないと登録できない' do
+      it '価格が1000万以上だと登録できない' do
+        @item.price = 10000000
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price must be less than 10000000")
       end
       it '価格は半角数字でないと登録できない' do
+        @item.price = "６００"
+        @item.valid?
+        expect(@item.errors.full_messages).to include ("Price is not a number")
+      end
+      it '画像がないと登録できない' do
+        @item.image = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Image can't be blank")
       end
     end
   end
