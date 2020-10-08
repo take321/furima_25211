e7d97ee0ab6ab6f02f1d5b27c51cebfd0ea14759
take321 <70375053+take321@users.noreply.github.com>
2020-10-08 19:32:27 +0900

    ブラウザのカード情報取得

---
+++ app/javascript/card.js
@@ -0,0 +1,18 @@
+const pay = ()=> {
+  Payjp.setPublicKey(process.env.PAYJP_PUBLIC_KEY);
+  const form = document.getElementById("charge-form");
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const formResult = document.getElementById("charge-form");
+    const formData = new FormData(formResult);
+
+    const card = {
+      number: formData.get("number"),
+      cvc: formData.get("cvc"),
+      exp_month: formData.get("exp_month"),
+      exp_year: `20${formData.get("exp_year")}`,
+    };
+  })
+}
+window.addEventListener("load", pay);
