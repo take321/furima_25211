ba705bf01188b4f964da0fdc89c7962aaedac998
take321 <70375053+take321@users.noreply.github.com>
2020-10-14 18:41:36 +0900

    テストコードの記述

--- spec/models/order_credit_spec.rb
+++ spec/models/order_credit_spec.rb
@@ -1,31 +1,57 @@
 require 'rails_helper'
 
 RSpec.describe OrderCredit, type: :model do
-  before
-    @order_credit =  FactoryBot.build(:order_credit)
+  before do
+   @order_credit =  FactoryBot.build(:order_credit)
   end 
   describe 'shipping_addressesテーブルへの保存' do
     context 'shipping_addressesテーブルへの保存がうまくいくとき' do
       it 'すべての欄が埋まっていれば登録できる' do
+        expect(@order_credit).to be_valid
       end
     end
 
     context 'shipping_addressesテーブルへの保存がうまくいかないとき' do
       it '郵便番号がないと登録できない' do
+        @order_credit.postal_code = ""
+        @order_credit.valid?
+        expect(@order_credit.errors.full_messages).to include("Postal code can't be blank")
       end
       it '郵便番号は「３文字ハイフン４文字」と入力されていなければ登録できない' do
+        @order_credit.postal_code = 1234-123
+        @order_credit.valid?
+        expect(@order_credit.errors.full_messages).to include("Postal code is invalid. Include hyphen(-)")
       end
-      it '都道府県が選ばれていないと選択できない' do
+      it '都道府県がプルダウンから選ばれていないと登録できない' do
+        @order_credit.prefecture_id = 1
+        @order_credit.valid?
+        expect(@order_credit.errors.full_messages).to include("Prefecture must be other than 1")
       end
       it '市区町村がないと登録できない' do
+        @order_credit.city_wards_towns = ""
+        @order_credit.valid?
+        expect(@order_credit.errors.full_messages).to include("City wards towns can't be blank")
       end
       it '番地がないと登録できない' do
+        @order_credit.address = ""
+        @order_credit.valid?
+        expect(@order_credit.errors.full_messages).to include("Address can't be blank")
       end
       it '電話番号がないと登録できない' do
+        @order_credit.phone = ""
+        @order_credit.valid?
+        expect(@order_credit.errors.full_messages).to include("Phone can't be blank")
       end
       it '電話番号が11文字に満たないと登録できない' do
+        @order_credit.phone = 1234567890
+        @order_credit.valid?
+        expect(@order_credit.errors.full_messages).to include("Phone is invalid")
       end
       it 'トークンが取得できていないと登録できない' do
+        @order_credit.token = ""
+        @order_credit.valid?
+        expect(@order_credit.errors.full_messages).to include("Token There is an error in the card information")
       end
+    end
   end
 end
